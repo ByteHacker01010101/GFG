ad0df47099266805d6333f6fad2e94d266839185
𝚂𝚊𝚖𝚙𝚛𝚒𝚝 𝙼𝚊𝚗𝚍𝚊𝚕 <149590176+ByteHacker01010101@users.noreply.github.com>
2026-07-24 09:05:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Consecutive Path in Binary tree/longest-consecutive-path-in-binary-tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int ans = 1;
+
+    void dfs(Node* root, Node* parent, int len) {
+        if (!root) return;
+
+        if (parent && root->data == parent->data + 1)
+            len++;
+        else
+            len = 1;
+
+        ans = max(ans, len);
+
+        dfs(root->left, root, len);
+        dfs(root->right, root, len);
+    }
+
+    int longestConsecutive(Node* root) {
+        if (!root) return -1;
+
+        ans = 1;
+        dfs(root, NULL, 1);
+
+        return (ans == 1) ? -1 : ans;
+    }
+};
