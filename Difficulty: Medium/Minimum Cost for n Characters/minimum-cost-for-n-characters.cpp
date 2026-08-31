2d520d979333d5d355025af298d44dbddcdc318e
𝚂𝚊𝚖𝚙𝚛𝚒𝚝 𝙼𝚊𝚗𝚍𝚊𝚕 <149590176+ByteHacker01010101@users.noreply.github.com>
2026-08-31 08:40:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Cost for n Characters/minimum-cost-for-n-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long minCost(int n, int i, int d, int c) {
+        vector<long long> dp(n + 1, 0);
+
+        dp[0] = 0;
+
+        for (int x = 1; x <= n; x++) {
+            dp[x] = dp[x - 1] + i;
+            if (x % 2 == 0) {
+                dp[x] = min(dp[x],
+                            dp[x / 2] + c);
+            }
+            else {
+                dp[x] = min(dp[x],
+                            dp[(x + 1) / 2] + c + d);
+            }
+        }
+
+        return dp[n];
+    }
+};
