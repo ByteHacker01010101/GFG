cb8389acd07c58f3d08c0f6da739a6e319c4efc4
𝚂𝚊𝚖𝚙𝚛𝚒𝚝 𝙼𝚊𝚗𝚍𝚊𝚕 <149590176+ByteHacker01010101@users.noreply.github.com>
2026-09-22 08:54:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Matching in Dictionary with Removals/longest-matching-in-dictionary-with-removals.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        int n = s.size();
+
+        
+        vector<vector<int>> pos(26);
+
+        for (int i = 0; i < n; i++) {
+            pos[s[i] - 'a'].push_back(i);
+        }
+
+        string ans = "";
+
+        for (const string& word : d) {
+            int prev = -1;
+            bool possible = true;
+
+            for (char c : word) {
+                auto &v = pos[c - 'a'];
+
+                auto it = upper_bound(v.begin(), v.end(), prev);
+
+                if (it == v.end()) {
+                    possible = false;
+                    break;
+                }
+
+                prev = *it;
+            }
+
+            if (possible) {
+                if (word.size() > ans.size() ||
+                    (word.size() == ans.size() && word < ans)) {
+                    ans = word;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
